24a5bd7992d6b8d6ef9343fca74cfc34ab2e9242
Mohammad Aarman <inn.mdaarman@gmail.com>
2025-10-19 01:32:45 +0545

    laravel Breeze Installed

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\VideoController;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/videos/upload', [VideoController::class, 'create']);
-Route::post('/videos/upload', [VideoController::class, 'store'])->name('videos.store');
- 
-Route::get('/videos', [VideoController::class, 'index'])->name('videos.index');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::get('/', [VideoController::class, 'home'])->name('home');
-
+require __DIR__.'/auth.php';
